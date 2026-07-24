c2104e2525c964b2fe9263ec825d3eefbede19c0
Dave Lerner <davidlerner@oneappguarantee.com>
2026-07-24 09:54:07 -0700

    Remove accidentally committed build artifacts; ignore target/

--- target/release/deps/jolta-8959473fc5544b57.d
+++
@@ -1,7 +0,0 @@
-/Users/dave/workspace/jolta/target/release/deps/jolta-8959473fc5544b57.d: src/main.rs
-
-/Users/dave/workspace/jolta/target/release/deps/jolta-8959473fc5544b57: src/main.rs
-
-src/main.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.2.0
--- target/release/jolta.d
+++
@@ -1,1 +0,0 @@
-/Users/dave/workspace/jolta/target/release/jolta: /Users/dave/workspace/jolta/src/main.rs
